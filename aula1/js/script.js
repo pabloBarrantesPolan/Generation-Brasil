723e76ad1e9a71a39e659a7b0bedd89029d08650
pabloBarrantesPolan <Digital@DH-A2-10.dhbrasil.digitalhouse.com>
2019-05-17 16:14:46 -0300

    adicionando conteudo das aulas Generation

---
+++ aula1/js/script.js
@@ -0,0 +1,13 @@
+//alert("oie!");
+ var geni = "ola!";
+//console.log(geni.length);
+geni="tudo bem?"
+for (var i = 0; i < geni.length; i++) {
+  console.log(geni.charAt(i));
+}
+console.log(geni.indexOf("l"));
+
+console.log(parseInt("genifer",32));
+console.log(parseInt("geni",32));
+console.log(parseInt("geni",10));
+console.log(parseInt("genifer",25));
